dd396ba48e75ffcbbb1b640717443fa3b7e59ff5
laurel25 <estangel47@gmail.com>
2025-05-08 09:51:57 -0400

    message

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -4,11 +4,6 @@ let width;
 function calculateArea() {
     length = parseFloat(document.getElementById('length').value);
     width = parseFloat(document.getElementById('width').value);
-   }
-
-function calculateArea() {
-    length = parseFloat(document.getElementById('length').value);
-    width = parseFloat(document.getElementById('width').value);
    
    let area = length * width;
    }
